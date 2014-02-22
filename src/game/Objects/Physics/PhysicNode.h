a935d3330682e063ff105ce65c6de85224028d14
Teivaz <o.kuznietsov@gmail.com>
2014-02-22 03:14:07 +0200

    Added fake with progress bars Added animation files Added ship config

--- src/game/Objects/Physics/PhysicNode.h
+++ src/game/Objects/Physics/PhysicNode.h
@@ -6,29 +6,29 @@ MakeShared(Shape);
 class PhysicNode : virtual public GameObject
 {
 public:
-	PhysicNode(const vector<vec2f>& mesh);
-	PhysicNode(const string& meshName);
-	virtual ~PhysicNode();
-	
-	virtual void					renderDebug();
-	virtual void                    setPosition(const vec2f& p);
-	virtual void                    setScale(const vec2f& s);
+    PhysicNode(const vector<vec2f>& mesh);
+    PhysicNode(const string& meshName);
+    virtual ~PhysicNode();
+    
+    virtual void                    renderDebug();
+    virtual void                    setPosition(const vec2f& p);
+    virtual void                    setScale(const vec2f& s);
     virtual void                    setRotation(float r);
-    virtual void					setAdditionalTransformation(const mat3f&);
-	virtual void					update(float dt);
-
-	virtual void					onCollided(PhysicNode* other, vec2f point);
-
-	const std::vector<vec2f>&		getMesh() const;
-	bool							hasMoved() const;
-
-	virtual void					setMass(float mass);
-	virtual float					getMass() const;
-	virtual void					setDirection(const vec2f& dir);
-	virtual const vec2f&			getDirection() const;
-	virtual void					setSpeed(float speed);
-	virtual float					getSpeed() const;
-
+    virtual void                    setAdditionalTransformation(const mat3f&);
+    virtual void                    update(float dt);
+    
+    virtual void					onCollided(PhysicNode* other, vec2f point);
+    
+    const std::vector<vec2f>&		getMesh() const;
+    bool							hasMoved() const;
+    
+    virtual void					setMass(float mass);
+    virtual float					getMass() const;
+    virtual void					setDirection(const vec2f& dir);
+    virtual const vec2f&			getDirection() const;
+    virtual void					setSpeed(float speed);
+    virtual float					getSpeed() const;
+    
     void                            setColliosionChecked();
     virtual float					getEnergy() const;
     float                           getSqBoundingRadius() const 
